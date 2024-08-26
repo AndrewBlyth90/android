0c568302321d41e292e396394d70a21b595290f2
Andrew Blyth <andrewblyth90@gmail.com>
2024-08-26 16:01:43 +0100

    adds in interactions

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -38,51 +43,98 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PageContainer(){
+
+    var result by remember {
+        mutableIntStateOf(1)
+    }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.doorstep
+        2 -> R.drawable.finn
+        3 -> R.drawable.jilly
+        4 -> R.drawable.morag
+        5 -> R.drawable.pathway
+        else -> R.drawable.sploot
+    }
+
+    val stringResource = when (result) {
+        1 -> R.string.doorstep
+        2 -> R.string.finn
+        3 -> R.string.jilly
+        4 -> R.string.morag
+        5 -> R.string.pathway
+        else -> R.string.sploot
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(painter = painterResource(
-            id = R.drawable.doorstep),
+            id = imageResource),
             contentDescription = "",
             modifier = Modifier
                 .width(400.dp)
                 .shadow(elevation = 10.dp)
         )
+
         Spacer(modifier = Modifier.height(30.dp))
-        TitleHolder()
+
+        TitleHolder(stringResource = stringResource)
+
         Spacer(modifier = Modifier.height(20.dp))
-        ButtonRow()
+
+        Row {
+            Button(onClick = {
+                result = orderChecker(result, "previous")
+            }, modifier = Modifier.width(150.dp)) {
+                Text("Previous")
+            }
+
+            Spacer(modifier = Modifier.width(40.dp))
+
+            Button(onClick = {
+                result = orderChecker(result, "next")
+            }, modifier = Modifier.width(150.dp)) {
+                Text("Next")
+            }
+        }
+
     }
 }
 
 @Composable
-fun TitleHolder(){
+fun TitleHolder(
+    @StringRes stringResource: Int
+){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id = R.string.doorstep)
+            text = stringResource(id = stringResource)
         )
         Text(text = stringResource(id = R.string.andrew))
     }
 }
 
-@Composable
-fun ButtonRow(){
-    Row {
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.width(150.dp)) {
-            Text("Previous")
+fun orderChecker (number: Int, buttonDirection: String ): Int {
+
+    if(buttonDirection == "previous"){
+        if(number == 1){
+            return 6
         }
-        Spacer(modifier = Modifier.width(40.dp))
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.width(150.dp)) {
-            Text("Next")
-        }
+        return number - 1
     }
+
+    if (number == 6){
+        return 1
+    }
+    if (number == 0){
+        return 1
+    }
+    return number + 1
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
