f72ed6e8c2d8d15e7c0cc9e7aad6fda34a8be808
Andrew Blyth <andrewblyth90@gmail.com>
2024-09-08 20:39:54 +0100

    completes app

--- 30DaysofMusic/app/src/main/java/com/example/a30daysofmusic/MainActivity.kt
+++ 30DaysofMusic/app/src/main/java/com/example/a30daysofmusic/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +36,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a30daysofmusic.model.Album
+import com.example.a30daysofmusic.model.albums
 import com.example.compose.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,27 +45,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AlbumItemCard(album =
-                    Album(
-                        artistNameRes = R.string.alice_in_chains,
-                        albumNameRes = R.string.dirt,
-                        albumDescriptionRes = R.string.dirt_description,
-                        albumImageRes = R.drawable.aic_dirt,
-                        day = 1
-                    ))
-                }
+                MusicApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MusicApp(){
+    Scaffold (
+        topBar = { MusicTopAppBar() }
+    ) { it ->
+        Box (
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LazyColumn (
+                contentPadding = it
+            ) {
+                items(albums) {
+                    AlbumItemCard(album = it)
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -100,18 +113,28 @@ fun AlbumItemCard(album: Album){
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MusicTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppTheme {
-        AlbumItemCard(
-            Album(
-            artistNameRes = R.string.alice_in_chains,
-            albumNameRes = R.string.dirt,
-            albumDescriptionRes = R.string.dirt_description,
-            albumImageRes = R.drawable.aic_dirt,
-            day = 1
-        )
-        )
+       MusicApp()
     }
 }
--- 30DaysofMusic/app/src/main/java/com/example/a30daysofmusic/model/Album.kt
+++ 30DaysofMusic/app/src/main/java/com/example/a30daysofmusic/model/Album.kt
@@ -10,7 +10,6 @@ data class Album(
     @StringRes val albumNameRes: Int,
     @StringRes val albumDescriptionRes: Int,
     @DrawableRes val albumImageRes: Int,
-    val day: Int
 
 )
 
@@ -20,6 +19,31 @@ val albums = listOf(
         albumNameRes = R.string.dirt,
         albumDescriptionRes = R.string.dirt_description,
         albumImageRes = R.drawable.aic_dirt,
-        day = 1
+    ),
+    Album(
+        artistNameRes = R.string.blink_182,
+        albumNameRes = R.string.take_off,
+        albumDescriptionRes = R.string.take_off_description,
+        albumImageRes = R.drawable.blink_182_take_off_your_pants_and_jacket_cover
+    ),
+    Album(
+        artistNameRes = R.string.black_sabbath,
+        albumNameRes = R.string.masters_of_reality,
+        albumDescriptionRes = R.string.masters_of_reality_desc,
+        albumImageRes = R.drawable.bs_masters,
+    ),
+    Album(
+        artistNameRes = R.string.danzig,
+        albumNameRes = R.string.lucifuge,
+        albumDescriptionRes = R.string.lucifuge_desc,
+        albumImageRes = R.drawable.danzig_lucifuge
+    ),
+    Album(
+        artistNameRes = R.string.death,
+        albumNameRes = R.string.symbolic,
+        albumDescriptionRes = R.string.symbolic_desc,
+        albumImageRes = R.drawable.death_symbolic,
     )
+
+
 )
