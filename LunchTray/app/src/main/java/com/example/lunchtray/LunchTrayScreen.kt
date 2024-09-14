befc3e3f6e105822e705a228814070843c794d2a
Andrew Blyth <andrewblyth90@gmail.com>
2024-09-14 13:18:12 +0100

    adds in app bar

--- LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -11,53 +11,101 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the Lic
+ * ense.
  */
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.ui.OrderViewModel
 import com.example.lunchtray.ui.StartOrderScreen
 
-enum class lunchtrayScreen(@StringRes val title: Int){
-    start(title = R.string.start_order),
-    entree(title = R.string.choose_entree),
-    side(title = R.string.choose_side_dish),
+enum class LunchTrayScreen(@StringRes val title: Int){
+    Start(title = R.string.start_order),
+    Entree(title = R.string.choose_entree),
+    Side(title = R.string.choose_side_dish),
     Accompaniment(title = R.string.choose_accompaniment),
-    checkout(title = R.string.order_checkout)
+    Checkout(title = R.string.order_checkout)
 }
 
-// TODO: AppBar
-
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayAppBar(
+    currentScreen: LunchTrayScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+
+    ){
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+        )
+}
+
 @Composable
 fun LunchTrayApp() {
 
     val navController = rememberNavController()
     val viewModel: OrderViewModel = viewModel()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.Start.name
+    )
 
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchTrayAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+                )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
-        NavHost(navController = navController, startDestination = lunchtrayScreen.start.name, modifier = Modifier.fillMaxSize()) {
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+            modifier = Modifier.fillMaxSize()
+        ) {
 
-            composable(route = lunchtrayScreen.start.name){
-                StartOrderScreen(onStartOrderButtonClicked = { /*TODO*/ })
+            composable(route = LunchTrayScreen.Start.name){
+                StartOrderScreen(onStartOrderButtonClicked = {
+                    navController.navigate(LunchTrayScreen.Entree.name)
+                })
+            }
+
+            composable(route = LunchTrayScreen.Entree.name){
+
             }
 
         }
